0dc909c4ea2f9d10fab52cac356ea3fd35945f25
Lajos Tamas <tamaslajossulis777@gmail.com>
2026-09-15 18:38:26 +0200

    Elso commit - sajat feladatok hozzaadasa

---
+++ sajat_feladatok/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sajat_feladatok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
